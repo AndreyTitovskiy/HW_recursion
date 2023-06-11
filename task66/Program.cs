e0d8f2aa4cbaaded793af8f1fbae26af32529183
andrew titovskiy <titovskiy_andrew@mail.ru>
2023-06-11 11:35:24 +0600

    ДЗ завершено

---
+++ task66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите натуральное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+if (numberM <= 0 || numberN <= 0) Console.WriteLine("Числa не являются натуральными.");
+else if (numberM < numberN)
+{
+    int sumNumbers = SumNaturalNumbers(numberM, numberN);
+    Console.Write($"M = {numberM} ; N = {numberN}. -> {sumNumbers}");
+}
+else
+{
+    int sumNumbers = SumNaturalNumbers(numberN, numberM);
+    Console.Write($"M = {numberM} ; N = {numberN}. -> {sumNumbers}");
+}
+
+int SumNaturalNumbers(int numM, int numN)
+{
+    if (numM == numN) return numM;
+    return numM + SumNaturalNumbers(numM + 1, numN);
+}
---
+++ task68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.Write("Введите неотрицательное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите неотрицательное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int result = Ackermann(numberM, numberN);
+Console.WriteLine($"Результат функции Аккермана для m = {numberM} и n = {numberN} равен {result}");
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return Ackermann(m - 1, 1);
+    return Ackermann(m - 1, Ackermann(m, n - 1));
+}
